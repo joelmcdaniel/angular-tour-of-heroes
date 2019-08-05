507fb2915a226ce86f3575a34362e905a47f5e98
joelmcdaniel <joel.mcdaniel@live.com>
2019-08-05 13:38:01 -0700

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+}
